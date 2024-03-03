f7c33b2724712c84782db361b34a7956836e1dfe
Ahmed Badawy Mohamed El Amir <amirbadawy68@gmail.com>
2024-03-03 04:07:47 +0200

    vid

--- lib/main.dart
+++ lib/main.dart
@@ -7,12 +7,12 @@ void main() {
   runApp(DevicePreview(
       enabled: true,
       builder: (context) {
-        return const MyApp();
+        return const ResponsiveDashBoard();
       }));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class ResponsiveDashBoard extends StatelessWidget {
+  const ResponsiveDashBoard({super.key});
 
   // This widget is the root of your application.
   @override
@@ -21,23 +21,8 @@ class MyApp extends StatelessWidget {
         locale: DevicePreview.locale(context),
         builder: DevicePreview.appBuilder,
         debugShowCheckedModeBanner: false,
-        title: 'Flutter Demo',
+        title: 'Responsive Dashboard',
         theme: ThemeData(
-          // This is the theme of your application.
-          //
-          // TRY THIS: Try running your application with "flutter run". You'll see
-          // the application has a purple toolbar. Then, without quitting the app,
-          // try changing the seedColor in the colorScheme below to Colors.green
-          // and then invoke "hot reload" (save your changes or press the "hot
-          // reload" button in a Flutter-supported IDE, or press "r" if you used
-          // the command line to start the app).
-          //
-          // Notice that the counter didn't reset back to zero; the application
-          // state is not lost during the reload. To reset the state, use hot
-          // restart instead.
-          //
-          // This works for code too, not just values: Most code changes can be
-          // tested with just a hot reload.
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
         ),
